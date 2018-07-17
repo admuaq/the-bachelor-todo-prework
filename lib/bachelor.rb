6a9219c7356daae0563ccec097ca138674f62f5c
Adham Muhammad <admuaq@gmail.com>
2018-07-17 06:10:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,23 @@
+require 'pry'
+
+
 def get_first_name_of_season_winner(data, season)
   # code here
-end
+  # bachelor #get_first_name_of_season_winner returns 'Tessa' when passed the data and 'season 10'
+  
+      name = data[season].detect {|person|  person['status'] == 'Winner'}
+      name['name'].split(' ')[0]
+      #binding.pry
+end 
 
 def get_contestant_name(data, occupation)
   # code here
+  
+    data.values do |key, value|
+      element = value.detect {|person| person['occupation'] == occupation}
+      element['name']
+      #binding.pry
+    end     
 end
 
 def count_contestants_by_hometown(data, hometown)
